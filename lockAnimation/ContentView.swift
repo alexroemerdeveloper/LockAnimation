f24c6197bb174544fc65e91e808267308c630cd2
Alexander Römer <alex@roemer.design>
2020-03-23 19:02:49 +0100

    initial commit

--- lockAnimation/ContentView.swift
+++ lockAnimation/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tilted = false
+    @State private var locked = false
+
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .frame(width: 64, height: 64)
+                .cornerRadius(6)
+                .foregroundColor(Color(.darkGray))
+            
+            Capsule()
+                .trim(from: locked ? 0 : 3/5, to: 1)
+                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+                .frame(width: 44, height: 44)
+                .foregroundColor(Color(.darkGray))
+                .offset(y: -30)
+                .animation(Animation.timingCurve(0.68, -0.6, 0.32, 1.6).delay(0.5).repeatCount(1, autoreverses: true))
+                .onAppear() {
+                    self.locked.toggle()
+                }
+        }
+        .rotationEffect(.degrees(tilted ? 0 : 30))
+        .animation(Animation.timingCurve(0.68, -0.6, 0.32, 1.6).delay(0.5).repeatCount(1, autoreverses: true))
+        .onAppear() {
+            self.tilted.toggle()
+        }
     }
 }
 
